4de95df6c541f50322f85fd9c461ce46a5f45489
Karol Troška <karol.troska@avast.com>
2018-03-02 09:20:44 +0100

    - versioned intf ref

--- crossmodule/types/pointer.h
+++ crossmodule/types/pointer.h
@@ -1,9 +1,6 @@
 #pragma once
 
 
-#include "query.h"
-
-
 namespace crossmodule
 {
 
@@ -11,26 +8,14 @@ namespace crossmodule
 template<typename intf_base_t>
 struct versioned_intf_ref
 {
-	// Empty preparation for fill later using derived ctor:
-	versioned_intf_ref():
-		m_requestedVersion(0),
-		m_pIntf(0)
-	{}
-
 	template<typename intf_requested_t>
-	versioned_intf_ref(intf_requested_t*& pIntf) :
-		m_requestedVersion(intf_requested_t::version),
-		m_pIntf(reinterpret_cast<intf_base_t**>(&pIntf))
-	{}
+	versioned_intf_ref(intf_requested_t*& pIntf);
 
-	void attach(intf_base_t* pIntf)
-	{
-		*m_pIntf = reinterpret_cast<intf_base_t*>(pIntf->query(intf_base_t::iid, m_requestedVersion));
-	}
+	bool attach(intf_base_t* pIntf);
 
 protected:
-	intf_base_t**	m_pIntf;
-	uint32_t		m_requestedVersion;
+	void**   m_pIntf;
+	uint32_t m_requestedVersion;
 };
 
 
--- crossmodule/types/query.h
+++ crossmodule/types/query.h
@@ -5,46 +5,47 @@
 
 namespace crossmodule
 {
+namespace detail {
+
+struct queryable_tag {};
+
+}
 
 template <uint64_t _iid>
 struct queryable
 {
+	using queryable_t = detail::queryable_tag;
+
 	static constexpr uint64_t iid = _iid;
 
 	virtual void * query(uint64_t iid, uint32_t min_version, uint32_t & real_version) = 0;
 };
 
-
 template <typename... Tn>
 struct query_impl;
 
+template <typename T, typename U>
+T * iid_cast(U * u) noexcept;
 
-template <>
-struct query_impl<>
-{
-	void * query(uint64_t /*iid*/, uint32_t /*min_version*/, uint32_t & /*real_version*/)
-	{
-		return nullptr;
-	}
-};
+template <typename T, typename U>
+T & iid_cast(U & u);
 
+namespace detail {
 
-template <typename T0, typename... Tn>
-struct query_impl<T0, Tn...>:
-	T0, 
-	query_impl<Tn...>
-{
-	void * query(uint64_t iid_, uint32_t min_version, uint32_t & real_version) override
-	{
-		if (T0::iid == iid_ && T0::version >= min_version)
-		{
-			real_version = T0::version;
-			return static_cast<T0 *>(this);
-		}
+template <typename Intf, typename = void>
+struct is_queryable:
+	std::false_type
+{};
 
-		return query_impl<Tn...>::query(iid_, min_version, real_version);
-	}
-};
+template <typename Intf>
+struct is_queryable<Intf, std::enable_if_t<std::is_same<typename Intf::queryable_t, detail::queryable_tag>::value>>:
+	std::true_type
+{};
+
+}
+
+template <typename Intf >
+constexpr bool is_queryable_v = detail::is_queryable<Intf>::value;
 
 
 }
